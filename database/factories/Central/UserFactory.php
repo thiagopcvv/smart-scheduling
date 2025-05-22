bced921f1748f305aa9130151a5d495e0afad99c
thiagopcvv <thiagopiresdovalle@hotmail.com>
2025-05-21 22:11:12 -0400

    fix: back

--- database/factories/Central/UserFactory.php
+++ database/factories/Central/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => 'Thiago Pires de Castro do Valle',
             'email' => 'thiagopiresdovalle@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('1234'),
             'remember_token' => Str::random(10),
         ];
     }
