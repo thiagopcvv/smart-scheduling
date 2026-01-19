d9cd3f6b99a0092341a5c137de0f5c15d5859329
thiagopcvv <thiagopiresdovalle@hotmail.com>
2026-01-18 23:26:11 -0400

    fix: exception message

--- app/Http/Requests/Auth/Tenant/LoginRequest.php
+++ app/Http/Requests/Auth/Tenant/LoginRequest.php
@@ -54,7 +54,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'cpf' => __('auth.failed'),
+                'cpf' => "CPF ou senha inválidos",
             ]);
         }
 
