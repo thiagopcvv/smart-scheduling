239e75c08bb0b957475031663daa198ff0b0c376
thiagopcvv <thiagopiresdovalle@hotmail.com>
2026-01-22 00:32:15 -0400

    TODO

--- resources/js/pages/Tenant/Dashboard/index.tsx
+++ resources/js/pages/Tenant/Dashboard/index.tsx
@@ -22,7 +22,9 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"></div>
+                    <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {/*TODO: Criar dashboard com indicadores para usuários padrão e admin*/}
+                    </div>
                 </div>
             </div>
         </AppLayout>
